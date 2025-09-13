f1918b101f733a65d0f771dec70d8de34fe3648e
brunodantas <bruno.welldm@gmail.com>
2025-09-13 00:10:30 -0300

    update app names to include 'cosmic.' prefix

--- cosmic/core/apps.py
+++ cosmic/core/apps.py
@@ -2,6 +2,5 @@ from django.apps import AppConfig
 
 
 class CoreAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "core"
+    name = "cosmic.core"
     label = "core"
